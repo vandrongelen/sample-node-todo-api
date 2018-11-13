040c2830dea5e9d4e61c53f50f0e05fba646601a
Kees van Drongelen <kees@visualspace.nl>
2018-11-13 16:27:18 +0100

    Add GET /todos/:id

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,50 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// var id = '5bead3a4c4a3824383440e461';
+
+// if (!ObjectID.isValid(id)) {
+//   console.log('ID not valid');
+// }
+
+// Todo.find({
+//   _id: id
+// }).then((todos) => {
+//   console.log('Todos', todos);
+// });
+
+// Todo.findOne({
+//   _id: id
+// }).then((todo) => {
+//   console.log('Todo', todo);
+// });
+
+// Todo.findById(id).then((todo) => {
+//   if (!todo) {
+//     return console.log('Id not found');
+//   }
+//   console.log('Todo by Id', todo);
+// }).catch((e) => {
+//   console.log(e);
+// });
+
+// User.findById -no user found - user found - no valid id
+
+// if (!ObjectID.isValid(id)) {
+//   console.log('ID not valid');
+// }
+
+User.findById('5beadde1fe824c11de6aa96b').then((user) => {
+  if (!user) {
+    return console.log('User not found');
+  }
+
+  console.log('User by Id', user);
+}).catch((e) => {
+  console.log(e);
+});  
+
+
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
@@ -24,6 +25,46 @@ app.post('/todos', (req, res) => {
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
     res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+// GET todos/123455
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(400).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+
+});
+
+app.post('/users', (req, res) => {
+  var user = new User({
+    email: req.body.email
+  });
+
+  user.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.get('/users', (req, res) => {
+  Todo.find().then((users) => {
+    res.send({users});
   }, (e) => {
     res.status(400).send(e);
   });
