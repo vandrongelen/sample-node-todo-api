18fa129d5a8cb90168e6586b8b5adb1c370e5773
Kees van Drongelen <kees@visualspace.nl>
2018-11-14 09:23:23 +0100

    Added GET /todos/:id tests

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,15 @@
 const expect = require('expect');
-const request = require('supertest'); 
+const request = require('supertest');
+const { ObjectID } = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos = [{
+  _id: new ObjectID(),
   text: 'First test todo'
 }, {
+  _id: new ObjectID(),
   text: 'Second test todo'
 }];
 
@@ -70,3 +73,31 @@ describe('GET /todos', () => {
       .end(done);
   })
 });
+
+describe('GET /todos/:id', () => {
+  it('should get a todo by Id', (done) => {
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(todos[0].text);
+      })
+      .end(done);
+  });
+
+  it('shoud return a 404 if todo not found', (done) => {
+    // make sure you get 404 back
+    request(app)
+      .get(`/todos/${new ObjectID().toHexString()}`)
+      .expect(400)
+      .end(done);
+  });
+
+  it('should return 404 for non-object ids', (done) => {
+    // /todos/123
+    request(app)
+      .get('/todos/123abc')
+      .expect(404)
+      .end(done);
+  });
+})
